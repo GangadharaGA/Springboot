11fc0d7f729e80644909a082e782cc318341b86f
GangadharaGA <94308264+GangadharaGA@users.noreply.github.com>
2024-02-16 13:11:13 +0530

    Delete learning-portal-complete-main directory

--- learning-portal-complete-main/src/main/java/com/gangadhara/learningportalnew/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gangadhara.learningportalnew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gangadhara.learningportalnew.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
