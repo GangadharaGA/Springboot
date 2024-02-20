541f9cc5a65dd6466ba39a52418dceecfaf0011c
GangadharaGA <94308264+GangadharaGA@users.noreply.github.com>
2024-02-20 20:08:17 +0530

    Update UserService.java
    Updated for pnly User

--- Spring-security/service/ProductService.java
+++ Spring-security/service/ProductService.java
@@ -24,28 +24,7 @@ public class ProductService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @PostConstruct
-    public void loadProductsFromDB() {
-        productList = IntStream.rangeClosed(1, 100)
-                .mapToObj(i -> Product.builder()
-                        .productId(i)
-                        .name("product " + i)
-                        .qty(new Random().nextInt(10))
-                        .price(new Random().nextInt(5000)).build()
-                ).collect(Collectors.toList());
-    }
-
-
-    public List<Product> getProducts() {
-        return productList;
-    }
-
-    public Product getProduct(int id) {
-        return productList.stream()
-                .filter(product -> product.getProductId() == id)
-                .findAny()
-                .orElseThrow(() -> new RuntimeException("product " + id + " not found"));
-    }
+  
 
 
     public String addUser(UserInfo userInfo) {
